e7660410084f79c85cc28e43737b7a14f7d47791
Andrey Aratov <madingame@yandex.ru>
2017-06-04 14:50:40 +0300

    second section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
     console.log('Alive!!!');
+    createBgfromAlbum();
 });
+
+
+
+
+function createBgfromAlbum() {
+    var albumCoverImg = $('.featured-music__current-albm img');
+    var srcAlbumCoverImg = albumCoverImg.attr('src');
+    albumCoverImg.parent().parent().parent().css('background-image', 'url(' + srcAlbumCoverImg + ')');
+}
